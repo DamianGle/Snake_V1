a3a083d3c4c1698f56e8adc02841b106b2aff8fe
damian <damian_g@damian>
2020-10-11 16:46:23 +0200

    Added timer.
    Signed-off-by: damian <damian_g@damian>

--- app/src/main/java/com/example/snake/Snake.kt
+++ app/src/main/java/com/example/snake/Snake.kt
@@ -1,12 +1,37 @@
 package com.example.snake
 
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class Snake: AppCompatActivity()
 {
+    private var counter = 0;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.snake_play)
+
+        startTimeCounter()
+    }
+
+    private fun startTimeCounter() {
+        val countTime: TextView = findViewById(R.id.countTime)
+
+        object : CountDownTimer(10000, 1000)
+        {
+            override fun onTick(millisUntilFinished: Long) {
+                countTime.text = counter.toString()
+                counter++
+
+                if(counter == 10)
+                    counter = 0;
+            }
+            override fun onFinish() {
+                    this.start()
+            }
+        }.start()
     }
 }
