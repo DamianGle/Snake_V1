642f83f82431d0994bf531fcef0d3672eb183b45
damian <damian_g@damian>
2020-10-18 11:41:34 +0200

    Added checkDeath() and inform in toast().
    Signed-off-by: damian <damian_g@damian>

--- app/src/main/java/com/example/snake/Snake.kt
+++ app/src/main/java/com/example/snake/Snake.kt
@@ -83,6 +83,8 @@ class Snake: AppCompatActivity()
     {
         snakeBody.moveTails();
         draw();
+        if(snakeBody.checkDeath())
+            toast("Dead")
     }
 
     private fun startTimeCounter() {
